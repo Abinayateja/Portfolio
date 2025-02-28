eb586fc38b9aef8eb3317ad981d1a8df8de3559d
Abinayateja <130354488+Abinayateja@users.noreply.github.com>
2025-02-28 17:01:07 +0530

    Update Services.js

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -6,9 +6,9 @@ function Services ({ isDarkMode }) {
             <div className="container">
                 <h1 className="title  ">My Services</h1>
                 <div className="card-con row">
-                    <ServiceCard isDarkMode={isDarkMode} icn = "fa-solid fa-code" skill = "Web Design" dis = "I learn full stack"/>
-                    <ServiceCard isDarkMode={isDarkMode} icn = "fa-solid fa-crop-simple" skill = "UI/UX Design" dis = "I learn full stack"/>
-                    <ServiceCard isDarkMode={isDarkMode} icn = "fa-brands fa-app-store" skill = "App Design" dis = "I learn full stack"/>
+                    <ServiceCard isDarkMode={isDarkMode} icn = "fa-solid fa-code" skill = "Web Design" dis = "Crafting modern, responsive, and visually stunning websites that enhance brand identity and user engagement."/>
+                    <ServiceCard isDarkMode={isDarkMode} icn = "fa-solid fa-crop-simple" skill = "UI/UX Design" dis = "Designing intuitive, user-friendly interfaces with seamless interactions to maximize user satisfaction and conversion rates."/>
+                    <ServiceCard isDarkMode={isDarkMode} icn = "fa-brands fa-app-store" skill = "App Design" dis = "Creating sleek, high-performance mobile and web app designs that deliver an exceptional user experience and functionality."/>
                 </div>
             </div>
         </div>
