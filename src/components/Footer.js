b26bf055087c22b1ef3aa19874539891dc33549d
Abinayateja <130354488+Abinayateja@users.noreply.github.com>
2025-02-28 06:21:04 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../pages/about.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Your Name. All rights reserved.</p>
+      <p>&copy; 2025 Anusha. All rights reserved.</p>
       <div className="footer-links fi">
         <a href="https://github.com/VAnusha22" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
         <a href="https://www.linkedin.com/in/anusha-valluri/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
