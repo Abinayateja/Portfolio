106a063486c4283bc886c7a665ed4f534fdd05fc
Abinayateja <130354488+Abinayateja@users.noreply.github.com>
2025-02-28 06:04:54 +0530

    Update ProjectCard.js

--- src/pages/ProjectCard.js
+++ src/pages/ProjectCard.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const ProjectCard = (props) => {
     return (
-            <div className="Pcard col-12 col-md-4">
-                <img src={props.img} alt=""/>
+            <div className="Pcard col-12 col-md-4 style={{
+                backgroundImage: `url(${props.img})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                color: "white"
+              }}">
                 
             </div>
     );
